e6bb1c02b376bdb5591a2d4b35bb62a965cefa26
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-05-20 15:07:14 -0300

    Ejercicio 5 Prefab and Instantiate
    Continuado

---
+++ Assets/ManipulacionCubos.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManipulacionCubos : MonoBehaviour
+{
+    public Text txtSeleccionado;
+    int cubo;
+    GameObject clone;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;
+
+    public void ActivarBtn1()
+    {
+        txtSeleccionado.text = "El Cubo 1 está activado";
+        cubo = 1;
+    }
+
+    public void ActivarBtn2()
+    {
+        txtSeleccionado.text = "El Cubo 2 está activado";
+        cubo = 2;
+    }
+
+    public void ActivarBtn3()
+    {
+        txtSeleccionado.text = "El Cubo 3 está activado";
+        cubo = 3;
+    }
+
+    public void Instanciar()
+    {
+        switch (cubo)
+        {
+            case 1:
+                clone = Instantiate(cube1);
+                break;
+
+            case 2:
+                clone = Instantiate(cube2);
+                break;
+
+            case 3:
+                clone = Instantiate(cube3);
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
